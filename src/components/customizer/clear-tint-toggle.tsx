3083fb279ac395953ed488a5cf7b630b4113200f
najmus-sakib-hossain <manfromexistence@proton.me>
2025-09-24 12:01:43 +0600

    Increase size of button in ClearTintToggle for improved visibility

--- src/components/customizer/clear-tint-toggle.tsx
+++ src/components/customizer/clear-tint-toggle.tsx
@@ -14,7 +14,7 @@ export function ClearTintToggle() {
                 size="icon"
                 onClick={() => setIsClear(!isClear)}
             >
-                <div className="relative border h-4 w-4 rounded-full">
+                <div className="relative border h-5 w-5 rounded-full">
                     <div className="absolute top-0 left-0 h-full w-1/2 bg-background rounded-tl-full rounded-bl-full" />
                     <div className="absolute z-50 border h-2 w-2 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         <div className="absolute top-0 left-0 h-full w-1/2 bg-foreground rounded-tl-full rounded-bl-full" />
